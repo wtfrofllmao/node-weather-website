1baf86552ad3c14b4ff5152bd262eea33a46cf92
Dex <wtfrofllmao@gmail.com>
2020-09-25 13:48:43 +0800

    Remove unncessary console log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Loading javascript file...')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('p#message-1')
